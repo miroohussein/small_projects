90cd24c3dd08055b20c72fc66a6f9e94fe48f8ab
MARWA HUSSEIN <miroohussein98@gmail.com>
2023-10-27 02:02:00 +0200

    bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:tryagain/module/home/home_screen.dart';
 import 'package:tryagain/module/login/login_screen.dart';
 import 'package:tryagain/module/users/users_screen.dart';
+import 'package:tryagain/shared/bloc_observer/bloc_obverver.dart';
 import 'module/bmi/bmi_calculater.dart';
 import 'module/counter/counter_screen.dart';
 import 'module/messngar/messngar_page.dart';
 import 'package:tryagain/shared/component/components.dart';
 
 void main() {
+  Bloc.observer = MyBlocObserver();
   runApp(MyApp());
 }
 
